bf8da92caec894734823db6181e5d4349450193a
Octavio Martinez <omartinez1618@gmail.com>
2024-08-03 01:39:24 -0400

    pdf autotable

---
+++ src/components/DownloadPdf.tsx
@@ -0,0 +1,51 @@
+"use client"
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+
+interface DownloadPdfProps {
+    title: string
+    items: any
+}
+
+export default function DownloadPdf({ title, items }: DownloadPdfProps) {
+
+    const handlerDownload = () => {
+        var obj = new jsPDF('landscape');
+        obj.setFontSize(18);
+        obj.text(title, 20, 20);
+        obj.setFontSize(12);
+        const r = obj?.autoTable({
+            startX: 30,
+            startY: 40,
+            head: [[
+                'strengths',
+                'weaknesses'
+            ]],
+            body: [
+                items?.strengths,
+                items?.weaknesses
+            ],
+            theme: 'grid'
+        })
+        obj?.autoTable({
+            startX: 30,
+            startY: 40 + Math.max( items?.strengths?.length, items?.weaknesses?.length ) * 10,
+            head: [[
+                'opportunities',
+                'threats'
+            ]],
+            body: [
+                items?.opportunities,
+                items?.threats
+            ],
+            theme: 'grid'
+        });
+        obj.save(`${title.replaceAll(' ','-')}.pdf`);
+    }
+
+    return <button className="bg-blue-400 text-white py-1 px-5 rounded" onClick={handlerDownload}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+        </svg>
+    </button>
+}
--- src/components/ProjectDescription.tsx
+++ src/components/ProjectDescription.tsx
@@ -1,30 +1,35 @@
 import { ChangeEvent } from "react"
-import DownloadButton from "./DownloadButton"
-import DocPDF from "./DocPDF"
 
 interface ProjectNameProps {
     title: string
     value: string
     items: FODATypes
+    primaryActions?: JSX.Element
+    secondaryActions?: JSX.Element
     onChange?: (value: string) => void
     onSend: () => void
 }
 
-export default function ProjectDescription( { title, value, items, onChange, onSend }:ProjectNameProps ) {
+export default function ProjectDescription( { value, primaryActions, secondaryActions, onChange, onSend }:ProjectNameProps ) {
 
     const handleChange = (event:ChangeEvent<HTMLTextAreaElement>) => {
         onChange && onChange(event.target.value)
     }
 
-    return <div className="flex flex-col gap-1 left-0 justify-between px-2 py-2 w-full shadow-[0_-1px_10px_slategray] sm:shadow-none">
+    return <div className="flex flex-col gap-1 left-0 justify-between px-2 py-2 w-full shadow-[0_-1px_10px_slategray] sm:shadow-none" >
         <textarea
             onChange={handleChange}
             value={value}
             className="outline-none w-full bg-slate-200 py-2 px-4 text-slate-800"
-            placeholder="Describe tu idea..." />
-        <div className="flex flex-row justify-end gap-2 items-center">
-            { title && <DownloadButton document={<DocPDF title={title} items={items} />}/>}
-            <button onClick={onSend} className="bg-blue-400 text-white py-1 px-5 rounded">Enviar</button>
+            placeholder="Ej: Fabrica de iglus en el desierto del sahara" />
+        <div className="flex flex-row sm:justify-between justify-end gap-2 items-center">
+            <span className="sm:flex hidden flex-row gap-1">
+                { secondaryActions }
+            </span>
+            <span className="flex flex-row items-center gap-1">
+                { primaryActions }
+                <button onClick={onSend} className="bg-blue-400 text-white py-1 px-5 rounded">Enviar</button>
+            </span>
         </div>
     </div>
 }
