922ecd742da411ef9bcf15f41e2616862bba6d17
Javier Martínez <143547061+Javierjmc@users.noreply.github.com>
2024-08-06 13:56:22 +0000

    mejorando modo oscuro

--- src/components/ButtonDarkMode.tsx
+++ src/components/ButtonDarkMode.tsx
@@ -1,12 +1,35 @@
 'use client'
-export default function ButtonDarkMode() {  
-    const handleDarkMode=(event)=>{
-        document.querySelector('html').classList.toggle('dark')
+import { useEffect, useState } from "react"
 
+export default function ButtonDarkMode() {
+    const [theme, setTheme]=useState(()=>{
+        if(window.matchMedia('(prefers-color-scheme:dark)').matches){
+            return 'dark'
+        }
+        return 'light'
+    })
+    useEffect(()=>{
+        if(theme == 'dark'){
+            document.querySelector('html')?.classList.add('dark')
+        } else {
+            document.querySelector('html')?.classList.remove('dark')
+        }
+    },[theme])
+    const handleChangeTheme=()=>{
+        setTheme((prevTheme) => prevTheme=='light'?'dark':'light' )
     }
+
+     
     return (
-        <div className="fixed top-4 right-4">
-            <input onChange={handleDarkMode} data-hs-theme-switch="" className="relative w-[3.25rem] h-7 bg-gray-100 checked:bg-none checked:bg-blue-600 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 ring-1 ring-transparent focus:border-gray-700 focus:ring-gray-700 focus:outline-none appearance-none before:inline-block before:size-6 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:shadow before:rounded-full before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 after:absolute after:end-1.5 after:top-[calc(50%-0.40625rem)] after:w-[.8125rem] after:h-[.8125rem] after:bg-no-repeat after:bg-[right_center] after:bg-[length:.8125em_.8125em] after:transform after:transition-all after:ease-in-out after:duration-200 after:opacity-70 checked:after:start-1.5 checked:after:end-auto" type="checkbox" id="darkSwitch"/>
+        <div className="fixed top-4 right-4 cursor-pointer hover:bg-slate-200 dark:hover:bg-slate-700 rounded-full p-1">  
+            <svg onClick={handleChangeTheme} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 stroke-gray-700 dark:hidden">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+            </svg>
+            <svg onClick={handleChangeTheme} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-6 stroke-gray-300 ${theme=='dark'? "block" : "hidden"}`}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+             </svg>
+
+
         </div>
     )
 }
